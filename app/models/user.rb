76e5216eaa2691021164922a8ea27f715eadca1c
red <wonseok@duck.com>
2025-03-15 23:36:32 +0900

    디스코드에서 이름 같은 유저뒤에 숫자 처리.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_validation :set_user_numbers, if: -> { !user_numbers.present? }
+
+  private
+
+  def set_user_numbers
+    other_users_with_same_username = User.where("lower(username) like lower(?)", "%#{username}%")
+    self.user_numbers = other_users_with_same_username.count +1
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_15_123624) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_15_140446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_15_123624) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.integer "user_numbers"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
