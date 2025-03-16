89250d0783a43a0fd55235407e0825d356ecde94
red <wonseok@duck.com>
2025-03-16 21:56:02 +0900

    set foreign key

--- db/migrate/20250316025356_create_servers.rb
+++ db/migrate/20250316025356_create_servers.rb
@@ -2,7 +2,7 @@ class CreateServers < ActiveRecord::Migration[8.0]
   def change
     create_table :servers do |t|
       t.string :name
-      t.belongs_to :owner, null: false, foreign_key: true
+      t.belongs_to :owner, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
