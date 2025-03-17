b26301f2cec4136eb5284424fbc2243f5e7ecf57
Wonseok Yang <wsyang@digicaps.com>
2025-03-17 16:23:36 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_16_025356) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_17_072246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "server_channels", force: :cascade do |t|
+    t.string "name"
+    t.bigint "server_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["server_id"], name: "index_server_channels_on_server_id"
+  end
 
   create_table "servers", force: :cascade do |t|
     t.string "name"
@@ -36,5 +44,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_16_025356) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "server_channels", "servers"
   add_foreign_key "servers", "users", column: "owner_id"
 end
